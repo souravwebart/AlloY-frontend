d2198cddab90ee9d27c5b2d804ae9afd8b716983
souravwebart <guptasourav536@gmail.com>
2022-10-14 02:30:07 +0530

    v3 updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,15 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Header from "../Component/Header/Header";
 import Footer from "../Component/Footer";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Alloy Frontend</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
